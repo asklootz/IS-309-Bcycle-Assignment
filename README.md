ece50d27a573369f2b33c18aa5a3d437dc3dfe91
asklootz <askl@uia.no>
2026-04-17 09:47:36 +0200

    Edited mockdata for data-format

--- README.md
+++ README.md
@@ -4,17 +4,17 @@ A repo for the Bcycle Assignement in IS-309_26 at University of Agder
 
 ## Standarized formatting for values, mostly identifiers:
 
-| Table | Variable  | Format    | Example| Alt. Format | Alt. Example | Other info |
-| :----: | :-------: | :------: | :----: | :---:       | :---:        | :---:      |
-| station | station_id | \*program\* + "_" + \*incrementing int\* | bcycle_bergen_0001| \*program_name*\ + "\_station\_" + \*incrementing int\* | bergen_station_0001 | __Main difference being it specifing "station" in ID__ |
-| station_dock | station_dock_id | \*station_id\* + "_" + \*incrementing int\* | bcycle_bergen_0001_1| \*station_id\* "_" \*incrementing int\* | bergen_station_0001_1 | __Should this containt dock?__ | 
-| station_status | station_status_id | "station_status_" + \*incrementing int\* | station_status_1 | \*station_id\* + "\_status\_" + \*incrementing int\* | bergen_station_0001_status_1 | __General incremening ID or ID that increments for it's specific station?__ |
-| bike_type | bike_type_id | \*first letter of the bike_type\* + "b_" \*incrementing int\* | eb_01 / sb_02 | \*bike_type\* + "_" \*incrementing int\* | electric_01 | __Try to keep the id simple and self-explanatory of type of bike / Does ID need to say it is a bike?__ |
-| bike | bike_id | \*program_name\* + "\_bike\_" + \*first letter of biketype*\ + \*incrementing int\* | bergen_bike_e0001 / bergen_bike_s0004 | \*program_name\* + "\_" + \*bike_type\* + "\_" +  \*incrementing int\* | bergen_electric_0001 | __Does ID need to say it is a bike?__ |
-| bike_status | bike_status_id | "bike_status_" + \*incrementing int\* | bike_status_5 | \*bike_id\* + \_status\_ + \*incrementing int\* | bergen_electric_0001_status_1 | __General incremening ID or ID that increments for it's specific station?__ |
-| trip | trip_id | \*incrementing int\* | 1412 | "trip_" + \*incrementing int\* | trip_1412 | __More informative ID than just numbers__ |
-| user | user_id | \*incrementing int\* | 44 | "user_" + \*incrementing int\* | user_44 | __More informative ID than just numbers__ |
-| bought_membership | purchase_id | \*incrementing int\* | 22 | "purchase_" + \*incrementing int\* | purchase_22 | __More informative ID than just numbers__ |
+| Table | Variable  | Format    | Example| Other info |
+| :----: | :-------: | :------: | :----: | :---:      |
+| station | station_id | \*program_name*\ + "\_station\_" + \*incrementing int\* | bergen_station_1 | __Main difference being it specifing "station" in ID__ |
+| station_dock | station_dock_id | \*station_id\* + "_" + \*incrementing int\* | bergen_station_1_1 | __One extra int seperated by an underscoure to count the dock__ | 
+| station_status | station_status_id | "station_status_" + \*incrementing int\* | station_status_1 | __General incremening ID for all updates__ |
+| bike_type | bike_type_id | \*bike_type\* + "_" \*incrementing int\* | electric_1 | __Try to keep the id simple and self-explanatory of type of bike__ |
+| bike | bike_id | \*program_name\* + "\_bike\_" + \*incrementing int\* | bergen_bike_1 | __Does ID need to say it is a bike?__ |
+| bike_status | bike_status_id | "bike_status_" + \*incrementing int\* | bike_status_5 | __General incremening ID for all updates__ |
+| trip | trip_id | "trip_" + \*incrementing int\* | trip_1412 | __More informative ID than just numbers__ |
+| user | user_id | "user_" + \*incrementing int\* | user_44 | __More informative ID than just numbers__ |
+| bought_membership | purchase_id | "purchase_" + \*incrementing int\* | purchase_22 | __More informative ID than just numbers__ |
 
 ### General questions and comments for formatting:
 - Best way to keep the ID simple and informative/descriptive for different tables?
@@ -30,6 +30,8 @@ A repo for the Bcycle Assignement in IS-309_26 at University of Agder
 ## Work to do for finishing all the queries needed:  
 ### PLSQL
  - [ ] Add all the procedures we made for Assignment 2 into the "plsql.sql" file and make sure it runs properly.
+ - [ ] Update the create station procedure so it will automatically create docks on creation
+ - [ ] Make sure it will update station if you also add more docks. 
 ### Other
  - [ ] Create a new file "view-quieries.sql" to save all the queries used to show the data we have made and other info about the database we need to show.
  - [ ] Create a sql-file to store all the different scripts in once place, and have them in the correct order (create scheam => create tables => create constraints => etc.)
--- mock_data.sql
+++ mock_data.sql
@@ -6,51 +6,51 @@ values ('bcycle_bergen', 'NO', 'bergen', 'bergen', 'support@bergen.bycle.com', '
 
 -- For bike_type-table:
 insert into bergen.bike_type (bike_type_id, program_id, maker, model, bike_type)
-values ('rb_01', 'bcycle_bergen', 'Trek', 'FX3', 'regular'),
-       ('eb_01', 'bcycle_bergen', 'Trek', 'FX3e', 'electric'),
-       ('sb_01', 'bcycle_bergen', 'Smart Co', 'SX1', 'smart'),
-       ('cb_01', 'bcycle_bergen', 'Riese', 'Multicharger', 'cargo');
+values ('regular_1', 'bcycle_bergen', 'Trek', 'FX3', 'regular'),
+       ('electric_1', 'bcycle_bergen', 'Trek', 'FX3e', 'electric'),
+       ('smart_1', 'bcycle_bergen', 'Smart Co', 'SX1', 'smart'),
+       ('cargo_1', 'bcycle_bergen', 'Riese', 'Multicharger', 'cargo');
 
 -- For bike-table:
 insert into bergen.bike (bike_id, bike_type_id, dock_id, date_acquired)
-values ('bergen_bike_e0001', 'eb_01', 'bcycle_bergen_0001_1', '2023-01-15'),
-       ('bergen_bike_e0002', 'eb_01', NULL, '2023-02-20'),
-       ('bergen_bike_r0001', 'rb_01', 'bcycle_bergen_0002_1', '2023-03-10'),
-       ('bergen_bike_s0001', 'sb_01', NULL, '2023-04-05'),
-       ('bergen_bike_c0001', 'cb_01', 'bcycle_bergen_0003_1', '2023-05-12');
+values ('bergen_bike_1', 'electric_1', 'bcycle_bergen_0001_1', '2023-01-15'),
+       ('bergen_bike_2', 'electric_1', NULL, '2023-02-20'),
+       ('bergen_bike_3', 'regular_1', 'bcycle_bergen_0002_1', '2023-03-10'),
+       ('bergen_bike_4', 'smart_1', NULL, '2023-04-05'),
+       ('bergen_bike_5', 'cargo_1', 'bcycle_bergen_0003_1', '2023-05-12');
 
 -- For bike_status-table:
 insert into bergen.bike_status (bike_status_id, bike_id, status, latitude, longitude, battery, remaining_range, total_distance)
-values ('bike_status_1', 'bergen_bike_e0001', 'available', 60.3913, 5.3221, 85, 50, 1200),
-       ('bike_status_2', 'bergen_bike_e0002', 'in_use', 60.3920, 5.3230, 60, 35, 800),
-       ('bike_status_3', 'bergen_bike_r0001', 'available', 60.3915, 5.3225, NULL, NULL, 500),
-       ('bike_status_4', 'bergen_bike_s0001', 'maintenance', 60.3918, 5.3228, 67, 69, 300),
-       ('bike_status_5', 'bergen_bike_c0001', 'available', 60.3922, 5.3232, NULL, NULL, 700);
+values ('bike_status_1', 'bergen_bike_1', 'available', 60.3913, 5.3221, 85, 50, 1200),
+       ('bike_status_2', 'bergen_bike_2', 'in_use', 60.3920, 5.3230, 60, 35, 800),
+       ('bike_status_3', 'bergen_bike_3', 'available', 60.3915, 5.3225, NULL, NULL, 500),
+       ('bike_status_4', 'bergen_bike_4', 'maintenance', 60.3918, 5.3228, 67, 69, 300),
+       ('bike_status_5', 'bergen_bike_5', 'available', 60.3922, 5.3232, NULL, NULL, 700);
 
 -- For station-table:
 insert into bergen.station (station_id, program_id, name, address, postal_code, latitude, longitude, total_capacity, available_docks)
-values ('bcycle_bergen_0001', 'bcycle_bergen', 'Bryggen', 'Bryggen 1', 5003, 60.3961, 5.3228, 4, 2),
-       ('bcycle_bergen_0002', 'bcycle_bergen', 'Torgallmenningen', 'Torgallmenningen 1', 5014, 60.3925, 5.3240, 3, 2),
-       ('bcycle_bergen_0003', 'bcycle_bergen', 'Byparken', 'Byparken 1', 5015, 60.3910, 5.3215, 3, 2);
+values ('bergen_station_1', 'bcycle_bergen', 'Bryggen', 'Bryggen 1', 5003, 60.3961, 5.3228, 4, 2),
+       ('bergen_station_2', 'bcycle_bergen', 'Torgallmenningen', 'Torgallmenningen 1', 5014, 60.3925, 5.3240, 3, 2),
+       ('bergen_station_3', 'bcycle_bergen', 'Byparken', 'Byparken 1', 5015, 60.3910, 5.3215, 3, 2);
 
 -- For station_dock-table:
 insert into bergen.station_dock (dock_id, station_id, dock_number, bike_id, bike_type, is_occupied, is_accepting_returns, is_accepting_renting)
-values ('bcycle_bergen_0001_1', 'bcycle_bergen_0001', 1, 'bergen_bike_e0001', 'electric', true, false, true),
-       ('bcycle_bergen_0001_2', 'bcycle_bergen_0001', 2, NULL, NULL, false, true, false),
-       ('bcycle_bergen_0001_3', 'bcycle_bergen_0001', 3, NULL, NULL, false, true, false),
-       ('bcycle_bergen_0001_4', 'bcycle_bergen_0001', 4, NULL, NULL, false, true, false),
-       ('bcycle_bergen_0002_1', 'bcycle_bergen_0002', 1, 'bergen_bike_r0001', 'regular', true, false, true),
-       ('bcycle_bergen_0002_2', 'bcycle_bergen_0002', 2, NULL, NULL, false, true, false),
-       ('bcycle_bergen_0002_3', 'bcycle_bergen_0002', 3, NULL, NULL, false, true, false),
-       ('bcycle_bergen_0003_1', 'bcycle_bergen_0003', 1, 'bergen_bike_c0001', 'cargo', true, false, true),
-       ('bcycle_bergen_0003_2', 'bcycle_bergen_0003', 2, NULL, NULL, false, true, false),
-       ('bcycle_bergen_0003_3', 'bcycle_bergen_0003', 3, NULL, NULL, false, true, false);
+values ('bergen_station_1_1', 'bergen_station_1', 1, 'bergen_bike_1', 'electric', true, false, true),
+       ('bergen_station_1_2', 'bergen_station_1', 2, NULL, NULL, false, true, false),
+       ('bergen_station_1_3', 'bergen_station_1', 3, NULL, NULL, false, true, false),
+       ('bergen_station_1_4', 'bergen_station_1', 4, NULL, NULL, false, true, false),
+       ('bergen_station_2_1', 'bergen_station_2', 1, 'bergen_bike_3', 'regular', true, false, true),
+       ('bergen_station_2_2', 'bergen_station_2', 2, NULL, NULL, false, true, false),
+       ('bergen_station_2_3', 'bergen_station_2', 3, NULL, NULL, false, true, false),
+       ('bergen_station_3_1', 'bergen_station_3', 1, 'bergen_bike_5', 'cargo', true, false, true),
+       ('bergen_station_3_2', 'bergen_station_3', 2, NULL, NULL, false, true, false),
+       ('bergen_station_3_3', 'bergen_station_3', 3, NULL, NULL, false, true, false);
 
 -- For station_status-table:
 insert into bergen.station_status (station_status_id, station_id, dock_id, available_docks, regular_bikes_available, electric_bikes_available, smart_bikes_available, cargo_bikes_available, is_accepting_returns, is_accepting_renting)
-values ('station_status_1', 'bcycle_bergen_0001', 'bcycle_bergen_0001_1', 3, 0, 1, 0, 0, true, true),
-       ('station_status_2', 'bcycle_bergen_0002', 'bcycle_bergen_0002_1', 2, 1, 0, 0, 0, true, true),
-       ('station_status_3', 'bcycle_bergen_0003', 'bcycle_bergen_0003_1', 2, 0, 0, 0, 1, true, true);
+values ('station_status_1', 'bergen_station_1', 'bergen_station_1_1', 3, 0, 1, 0, 0, true, true),
+       ('station_status_2', 'bergen_station_2', 'bergen_station_2_1', 2, 1, 0, 0, 0, true, true),
+       ('station_status_3', 'bergen_station_3', 'bergen_station_3_1', 2, 0, 0, 0, 1, true, true);
 
 -- For user_info-table:
 insert into bergen.user_info (name, surname, email, date_of_birth, address, city, postal_code, state, total_trips)
@@ -68,5 +68,5 @@ values ('daily', 10, 1), ('weekly', 30, 7), ('monthly', 100, 30), ('yearly', 300
 
 -- For bought_membership-table:
 insert into bergen.bought_membership (user_id, membership_type, is_active, purchase_time, activation_time, expiration_time)
-values ('1', 'monthly', true, '2024-01-01 12:11:34.132', '2026-04-01 15:00:00', '2026-01-31 15:00:00'),
+values ('1', 'monthly', true, '2024-01-01 12:11:34.132', '2026-01-01 15:00:00', '2026-01-31 15:00:00'),
 ('2', 'weekly', true, '2024-01-15 11:22:45.678', '2026-04-09 12:00:00', '2024-04-16 12:00:00');
